d2d27bc1721304f305fbcdc2462dfee95167413d
Samir Cabrera <samircm1406@gmail.com>
2020-12-08 15:23:07 +0100

    first commit

---
+++ src/test/java/com/svalero/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
